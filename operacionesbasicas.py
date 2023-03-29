557fb4accf441b935eba70145c21dd3c3e9014d0
jennyillescas <tatillescas@gmail.com>
2023-03-28 21:54:34 -0500

    operacionesbasicas

---
+++ operacionesbasicas.py
@@ -0,0 +1,10 @@
+dato1=float(input("Ingrese un valor: "))
+dato2=float(input("Ingrese un valor: "))
+suma=dato1+dato2
+resta=dato1-dato2
+division=dato1/dato2
+multiplicacion=dato1*dato2
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multiplicacion)
+print("La divisiones: ", division)
